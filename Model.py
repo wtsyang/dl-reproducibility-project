2c22bbfc80eec1bbf20d9938e4a1c80df1f3b725
wtsyang <evily.yang@gmail.com>
2020-03-05 11:45:33 +0100

    Build the model C

--- Model.py
+++ Model.py
@@ -12,27 +12,40 @@ class Model(nn.Module):
         self.dropOut=dropOut
         self.BN=BN
 
-        # Debug
-        if np.sum(self.baseModel)>=2:
+        self.convInput = nn.Conv2d(input_size, 96, 3, padding=1)
+        self.conv_3_96_96_1 = nn.Conv2d(96, 96, 3, padding=1)
+        self.conv_3_96_96_2 = nn.Conv2d(96, 96, 3, padding=1, stride=2)
+
+        self.conv_3_96_192_1 = nn.Conv2d(96, 192, 3, padding=1)
+        self.conv_3_192_192_1 = nn.Conv2d(192, 192, 3, padding=1)
+        self.conv_3_192_192_2 = nn.Conv2d(192, 192, 3, padding=1, stride=2)
+
+        self.conv_1_192_192 = nn.Conv2d(192, 192, 1)
+        self.conv_1_192_class = nn.Conv2d(192, self.n_classes, 1)
+
+        self.BN_96 = nn.BatchNorm2d(96)
+        self.BN_192 = nn.BatchNorm2d(192)
+
+        self.MaxP = nn.MaxPool2d(3, stride=2)
+        self.softMax=nn.Softmax()
+        self.flatten=nn.Flatten()
+
+
+
+        # Debug of baseModel
+        if np.sum(self.baseModel)>=2 or np.sum(self.baseModel)==0:
             print('Error in baseModel. Choose model A')
             self.baseModel[0]=True
             self.baseModel[1]=False
             self.baseModel[2]=False
 
-        elif np.sum(self.baseModel)==0:
-            print('Error in baseModel. Choose model A')
-            self.baseModel[0] = True
-
-        if np.sum(self.modifiedModel) >= 2:
+        # Debug of modifiedModel
+        if np.sum(self.modifiedModel) >= 2 or np.sum(self.modifiedModel) == 0:
             print('Error in modifiedModel. Choose model without modification')
             self.modifiedModel[0] = True
             self.modifiedModel[1] = False
             self.modifiedModel[2] = False
             self.modifiedModel[3] = False
-
-        elif np.sum(self.modifiedModel) == 0:
-            print('Error in modifiedModel. Choose model without modification')
-            self.modifiedModel[0] = True
 
 
 
@@ -47,29 +60,73 @@ class Model(nn.Module):
 
     def __buildModelC(self,x):
 
-        convInput=nn.Conv2d(input_size, 96, 3, padding=1)
-        conv_3_96_96_1=nn.Conv2d(96, 96, 3, padding=1)
-        conv_3_96_96_2=nn.Conv2d(96, 96, 3, padding=1, stride=2)
-
-        conv_3_96_192_1=nn.Conv2d(96, 192, 3, padding=1)
-        conv_3_192_192_1=nn.Conv2d(192, 192, 3, padding=1)
-        conv_3_192_192_2=nn.Conv2d(192, 192, 3, padding=1, stride=2)
-
-        conv_1_192_192 = nn.Conv2d(192, 192, 1)
-        conv_1_192_class = nn.Conv2d(192, self.n_classes, 1)
-        BN_96=nn.BatchNorm2d(96)
-        BN_192=nn.BatchNorm2d(192)
-        #BN_class=nn.BatchNorm2d(self.n_classes)
-
         if seld.dropOut:
             x = F.dropout(x, .2)
-        x=convInput(x)
+
+        # Layer 1
+        x=self.convInput(x)
         if self.BN:
-            x=BN_96(x)
+            x=self.BN_96(x)
         x=F.relu(x)
-        
 
+        # Layer 2
+        x = self.conv_3_96_96_1(x)
+        if self.BN:
+            x = self.BN_96(x)
+        x = F.relu(x)
 
+        # Layer 3
+        x = self.conv_3_96_96_1(x)
+        if self.BN:
+            x = self.BN_96(x)
+        x = F.relu(x)
+
+        # Max Pooling
+        x =self.MaxP(x)
+        if seld.dropOut:
+            x = F.dropout(x, .5)
+
+        # Layer 4
+        x=self.conv_3_96_192_1(x)
+        if self.BN:
+            x = self.BN_192(x)
+        x = F.relu(x)
+
+        # Layer 5
+        x = self.conv_3_192_192_1(x)
+        if self.BN:
+            x = self.BN_192(x)
+        x = F.relu(x)
+
+        # Max Pooling
+        x = self.MaxP(x)
+        if seld.dropOut:
+            x = F.dropout(x, .5)
+
+        # Layer 6
+        x = self.conv_3_192_192_1(x)
+        if self.BN:
+            x = self.BN_192(x)
+        x = F.relu(x)
+
+        # Layer 7
+        x = self.conv_1_192_192_1(x)
+        if self.BN:
+            x = self.BN_192(x)
+        x = F.relu(x)
+
+        # Layer 8
+        x = self.conv_1_192_class(x)
+        if self.BN:
+            x = self.BN_192(x)
+        x = F.relu(x)
+
+        # The Last Layers
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = self.flatten(x)
+        x=self.softMax(x)
+
+        return x
 
 
 
